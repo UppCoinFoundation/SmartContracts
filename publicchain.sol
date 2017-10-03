edd1a6035783ddaad72ec24f0eb229edb4a7a9de
UppCoin <30726727+UppCoin@users.noreply.github.com>
2017-10-03 11:45:15 +0200

    Create publicchain.sol

---
+++ publicchain.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.0;
+import "./POAtest2.sol";
+contract TrackBalance {
+ 
+    address private _trustedaddress;
+
+    function ProofofAuthorityTracker(address p) {
+        _trustedaddress= p;
+    }
+    
+    function processTransaction(uint256 _value) returns (int256) {
+        log0("processTransaction called");
+
+        // only allow trusted address
+        if (msg.sender == _trustedaddress) {
+            log1("processTransaction address _to, ", bytes32(_value));
+            return 1;
+        }
+
+        log0("processTransaction failed");
+        return 0;
+    }
+}
